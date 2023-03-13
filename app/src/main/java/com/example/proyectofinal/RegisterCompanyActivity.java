8b8d073490ba291373c265d3dbec34e72e9f589b
BorjaMontoro <bmontoroplaza.cf@iesesteveterradas.cat>
2023-03-13 16:15:39 +0100

    Creado activity registro empresa

--- app/src/main/java/com/example/proyectofinal/RegisterCompanyActivity.java
+++ app/src/main/java/com/example/proyectofinal/RegisterCompanyActivity.java
@@ -1,28 +1,16 @@
 package com.example.proyectofinal;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterCompanyActivity extends AppCompatActivity {
     EditText email, name, surname, phone, password, compPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register_company);
 
         /*email = findViewById(R.id.registerEmail);
         name = findViewById(R.id.registerPersonName);
