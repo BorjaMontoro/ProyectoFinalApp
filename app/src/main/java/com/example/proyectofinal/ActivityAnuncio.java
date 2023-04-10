f981915abbaa20795f0de416a8e89d33af25dc26
pmunuera <pmunueragarcia.cf@iesesteveterradas.cat>
2023-04-10 22:26:02 +0200

    Commit N' Push

---
+++ app/src/main/java/com/example/proyectofinal/ActivityAnuncio.java
@@ -0,0 +1,47 @@
+package com.example.proyectofinal;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityAnuncio extends AppCompatActivity {
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                        }
+                    }
+                });
+    }
+    public void openSomeActivityForResult(View view) {
+        //Create Intent
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        //Launch activity to get result
+        //ImageView img = findViewById(R.id.img);
+        someActivityResultLauncher.launch(intent);
+    }
+}
